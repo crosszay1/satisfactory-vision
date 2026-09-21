a9da938434bfcd5c15ca9c88b2f264c6526d1c1a
crosszay <>
2026-09-21 15:47:27 -0400

    Finish gausion noise func

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Helpful resources used:
 https://www.youtube.com/watch?v=VIvYxnhkvvc
 https://github.com/Jackson-Kang/Pytorch-VAE-tutorial
 https://www.youtube.com/watch?v=iv-5mZ_9CPY&t=1803s
+
+concepts learned:
+Diffusion
+Gaussian noise
+NAE encoding
+LoRA
+Latent spaces
+Gaussian Distribution
--- src/experimentation/gaussian_noise.py
+++ src/experimentation/gaussian_noise.py
@@ -5,11 +5,29 @@ import cv2
 
 def add_gaussian_noise(sigma, mean, image_path):
     """
-    Sigma: Average value of the Gausian nosise, if graphed, it is the vertex
-    Mean: Deviation from the sigma
+    Sigma: Deviation from the sigma, the width of the graph
+    Mean: Average value of the Gausian nosise, if graphed, it is the vertex
     Image: Image to add noise to
     """
     image = cv2.imread(image_path)
     if not image:
         raise ValueError("Image not found")
     
+    height, width, channels = image.shape # Get shape
+
+    """
+    Create Gaussian noise (method 1, numpy)
+    noise = np.random.normal(mean, sigma, dimensions).astype(np.uint8)
+    """
+
+    # using method 1 here
+    noise = np.random.normal(mean, sigma, (height, width, channels)) #Insert height, width, image.shape
+
+    noisy_image = image + noise
+
+    noisy_image_clipped = np.clip(noisy_image, 0, 255) # Stay within bounds of 0-255
+
+    # Convert back to unsigned 8-bit integer type
+    noisy_image_clipped_u8 = noisy_image_clipped.astype(np.uint8)
+
+    return noisy_image_clipped_u8
