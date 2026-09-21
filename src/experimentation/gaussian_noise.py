a33c5527e2ce67af874ab15eaaf83416120ca07f
crosszay <>
2026-09-21 14:31:30 -0400

    Load img

--- src/experimentation/gaussian_noise.py
+++ src/experimentation/gaussian_noise.py
@@ -1,10 +1,15 @@
 import math
 import random
 import numpy as np
+import cv2
 
-def add_gaussian_noise(sigma, mean, image):
+def add_gaussian_noise(sigma, mean, image_path):
     """
     Sigma: Average value of the Gausian nosise, if graphed, it is the vertex
     Mean: Deviation from the sigma
     Image: Image to add noise to
     """
+    image = cv2.imread(image_path)
+    if not image:
+        raise ValueError("Image not found")
+    
