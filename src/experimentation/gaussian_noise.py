b04856f5f2d796025d662356a68b9208062b8c23
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2026-09-23 09:54:00 -0400

    Implement main block for Gaussian noise function
    Added a main block to test the add_gaussian_noise function with a sample image.

--- src/experimentation/gaussian_noise.py
+++ src/experimentation/gaussian_noise.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import cv2
 
+
 def add_gaussian_noise(sigma, mean, image_path):
     """
     Sigma: Deviation from the sigma, the width of the graph
@@ -12,8 +13,8 @@ def add_gaussian_noise(sigma, mean, image_path):
     image = cv2.imread(image_path)
     if not image:
         raise ValueError("Image not found")
-    
-    height, width, channels = image.shape # Get shape
+
+    height, width, channels = image.shape  # Get shape
 
     """
     Create Gaussian noise (method 1, numpy)
@@ -21,13 +22,22 @@ def add_gaussian_noise(sigma, mean, image_path):
     """
 
     # using method 1 here
-    noise = np.random.normal(mean, sigma, (height, width, channels)) #Insert height, width, image.shape
+    noise = np.random.normal(
+        mean, sigma, (height, width, channels)
+    )  # Insert height, width, image.shape
 
     noisy_image = image + noise
 
-    noisy_image_clipped = np.clip(noisy_image, 0, 255) # Stay within bounds of 0-255
+    noisy_image_clipped = np.clip(noisy_image, 0, 255)  # Stay within bounds of 0-255
 
     # Convert back to unsigned 8-bit integer type
     noisy_image_clipped_u8 = noisy_image_clipped.astype(np.uint8)
 
     return noisy_image_clipped_u8
+
+
+if __name__ == "__main__":
+    noisy_img = add_gaussian_noise(
+        sigma=10, mean=10, image_path="src/experimentation/rainbow.jpg"
+    )
+    cv2.imwrite("src/experimentation/rainbow_noisy.jpg", noisy_img)
