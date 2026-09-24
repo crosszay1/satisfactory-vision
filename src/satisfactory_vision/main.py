a7a3927db07a0f3e3c52241c42844d574ba0f6af
Isaiah <isaiahbfreundlich@gmail.com>
2026-09-24 15:12:10 -0400

    Add mean squared error loss function for the GAN. This will be used to train the generator and discriminator networks. The loss function will take in the predicted output from the generator and the true output from the discriminator, and compute the mean squared error between them. This will help the generator learn to produce outputs that are closer to the true outputs, and help the discriminator learn to distinguish between real and fake outputs.

--- src/satisfactory_vision/main.py
+++ src/satisfactory_vision/main.py
@@ -21,13 +21,29 @@ def satisfactory_to_rl(img): # Outputs image
 def rl_to_satisfactory(img): # Outputs image
     pass
 
+def calculate_mean_squared_error(img1, img2):
+    """
+    Calculate the mean squared error between two images.
+    """
+    return np.mean((img1 - img2) ** 2)
+
 # Rough format
 imgs = ["path", "path"]
 for img in imgs:
     img = cv2.imread(img)
-    img = satisfactory_to_rl(img)
-    img = add_gaussian_noise(img)
-    img = rl_to_satisfactory(img)
+    satis_img = satisfactory_to_rl(img)
+    satis_img_noisy = add_gaussian_noise(img)
+    rl_img = rl_to_satisfactory(img)
+
+    # If they differ too much, SMITE them
+    mse = calculate_mean_squared_error(satis_img, rl_img) #MSE = mean squared error
+    if mse > someNumber:
+        # Punish the generator
+        pass
+    if mse < someNumber:
+        # Reward the generator
+        pass
+    
 
     discrim_output_int = discriminator(img)
     discrim_output_bool = True if discrim_output >= 0.5 else False
