089edfa50a6cbe25090142503146ce11c770f9bf
Isaiah <isaiahbfreundlich@gmail.com>
2026-09-24 15:08:59 -0400

    Add pseudo code

--- src/satisfactory_vision/main.py
+++ src/satisfactory_vision/main.py
@@ -13,4 +13,34 @@ def add_gaussian_noise(img, sigma=25/255, mean=0.0, rng=None):
 Alright, let's try GAN
 """
 
+def discriminator(img): -> int
+    pass
 
+def satisfactory_to_rl(img): # Outputs image
+    pass
+def rl_to_satisfactory(img): # Outputs image
+    pass
+
+# Rough format
+imgs = ["path", "path"]
+for img in imgs:
+    img = cv2.imread(img)
+    img = satisfactory_to_rl(img)
+    img = add_gaussian_noise(img)
+    img = rl_to_satisfactory(img)
+
+    discrim_output_int = discriminator(img)
+    discrim_output_bool = True if discrim_output >= 0.5 else False
+
+    if discrim_output_bool == False && imageWasSatisfactory(): # See if discriminator was right
+        # If image was satisfactory, and discrim correctly identified reward generator, punish discrim
+    elif discrim_output_bool == True && not imageWasSatisfactory(): # See if discriminator was right
+        # If image was not satisfactory, but discrim incorrectly identified it as satisfactory, punish discrim
+    elif discrim_output_bool == True && imageWasSatisfactory(): # See if discriminator was right
+        # If image was satisfactory, and discrim correctly identified reward generator, reward discrim, punish gen
+    elif discrim_output_bool == False && not imageWasSatisfactory(): # See if discriminator was right
+        # If image was not satisfactory, and discrim correctly identified it as not satisfactory, reward discrim, punish gen
+
+
+
+    
