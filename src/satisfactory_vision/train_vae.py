fb3ec63fb4b8a2b92bdb67a0aa8b3f268036146d
Isaiah <isaiahbfreundlich@gmail.com>
2026-09-25 09:37:31 -0400

    Shape of VAE encoder

--- src/satisfactory_vision/train_vae.py
+++ src/satisfactory_vision/train_vae.py
@@ -1,8 +1,45 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as functional
+from pathlib import Path
 
+def load_image(image_path) -> torch.Tensor:
+    """
+    Loads an image from the given path and converts it to a PyTorch tensor.
+    """
+    image = cv2.imread(image_path)
+    if image is None:
+        raise ValueError("Image not found")
+    
+    # Convert BGR to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Normalize the image to [0, 1]
+    image = image.astype(np.float32) / 255.0
+    
+    # Compress to 128x128 pixels
+    image = cv2.resize(image, (128, 128))
+
+    # Convert to PyTorch tensor and add batch dimension
+    tensor = torch.from_numpy(image).permute(2, 0, 1)
+
+    return tensor
+def get_image_paths(directory: str) -> list[Path]:
+    """
+    Returns a list of image paths in the given directory.
+    """
+    folder = Path(directory)
+
+    file_array = [str(file) for file in folder_path.rglob("*") if file.is_file()]
+
+    return file_array
 class VAE(nn.Module):
+    def __init__(self, latent_dim=2): # Two dimensional latent space. output will be a 2d tensor
+        super(VAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = self.Encoder(latent_dim)
+        self.decoder = self.Decoder(latent_dim)
+
     def encode(self, tensor: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Encodes the input tensor into a mean and a log variance.
@@ -18,4 +55,14 @@ class VAE(nn.Module):
         Calculates the loss for the VAE.
         """
         pass
-    
+    def forward(self, x):
+        pass
+
+
+def main():
+    # Get all images paths
+    image_paths = get_image_paths("data")
+    print(image_paths)
+
+if __name__ == "__main__":
+    main()
